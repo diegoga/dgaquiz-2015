cd63dd65dcbc7f4d19a90bd0a09daaf52fab989a
Diego González Álvarez <gadiego@gmail.com>
2015-06-06 19:31:51 +0200

    Base de datos inicializada

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,21 @@
+var models=require('../models/models.js');
+
 //GET /quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: '¿Cuál es la capital del Reino Unido?'});
+	models.Quiz.findAll().success(function(quiz) {
+		res.render('quizes/question', {pregunta: quiz[0].pregunta})
+	})
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res){
-	if ((req.query.respuesta === 'Londres') || (req.query.respuesta === 'LONDRES')){
+	models.Quiz.findAll().success(function(quiz) {
+	if ((req.query.respuesta === quiz[0].respuesta)){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 	} else {
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
+ })
 };
 exports.author = function(req, res){
 	res.render('author/author', {autor: 'Diego González Álvarez'});
