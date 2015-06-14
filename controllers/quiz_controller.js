f14339595940d1325156c04196456826102002e8
Diego González Álvarez <gadiego@gmail.com>
2015-06-14 11:58:01 +0200

    Editar regidtros

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,19 +11,19 @@ exports.load = function(req, res, next, quizId){
 
 exports.index = function(req,res) {
 	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index', {quizes: quizes});
-	}).catch(function(error) {next(error);})
+		res.render('quizes/index', {quizes: quizes, errors: []});
+	}).catch(function(error) {next(error)})
 };
 //Búsqueda, search
 exports.busquedas = function(req, res){
 	var busc = '%'+req.query.busqueda.replace(/ /g, '%')+'%';
 	models.Quiz.findAll({where:["pregunta like ?", busc]}).then(function(quizes) {
-		res.render('busquedas/busquedas', {quizes: quizes, fallo: req.query.busqueda});
+		res.render('busquedas/busquedas', {quizes: quizes, fallo: req.query.busqueda, errors: []});
 		}).catch(function(error) { next(error);});
 };
 
 exports.show = function(req, res){
-		res.render('quizes/show', {quiz: req.quiz});
+		res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/answer
@@ -32,23 +32,53 @@ exports.answer = function(req, res){
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado='Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 exports.new = function(req, res){
 	var quiz=models.Quiz.build(
 		{pregunta: 'Pregunta', respuesta: 'Respuesta'}
 		);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 exports.create = function(req, res){
 	var quiz=models.Quiz.build(req.body.quiz);
-	quiz.save({fields: ['pregunta', 'respuesta']}).then(function(){
-		res.redirect('/quizes');
-	})
+	var errors=quiz.validate();
+	if (errors) {
+		var i=0; var errores=new Array();
+		for(var prop in errors) errores[i++]={message:errors[prop]};
+			res.render('quizes/new', {quiz: quiz, errors: errores});
+		} else {
+			quiz
+			.save({fields: ['pregunta', 'respuesta']})
+			.then(function(){ res.redirect('/quizes')})
+		}
+	};
+
+exports.edit = function(req, res){
+	var quiz=req.quiz;
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
 };
 
+exports.update = function(req, res){
+	req.quiz.pregunta=req.body.quiz.pregunta;
+	req.quiz.respuesta=req.body.quiz.respuesta;
+
+	var errors=req.quiz.validate();
+	if (errors) {
+		var i=0; var errores=new Array();
+		for(var prop in errors) errores[i++]={message:errors[prop]};
+			res.render('quizes/edit', {quiz: req.quiz, errors: errores});
+		} else {
+			req.quiz
+			.save({fields: ['pregunta', 'respuesta']})
+			.then(function(){ res.redirect('/quizes')})
+		}
+	};
+
+
 exports.author = function(req, res){
-	res.render('author/author', {autor: 'Diego González Álvarez'});
+	res.render('author/author', {autor: 'Diego González Álvarez', errors: []});
 };
